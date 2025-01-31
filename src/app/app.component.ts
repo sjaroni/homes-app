221ea133d40ec3de58f3d591db05d7498ce5b751
Stefan Jaroni <sjaroni@outlook.de>
2025-01-31 14:04:02 +0100

    add example-datas, components and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello Universe!</h1>`,
+  template: `<main>
+    <header class="brand-name">
+      <img
+        src="/assets/logo.svg"
+        alt="logo"
+        class="brand-logo"
+        aria-hidden="true"
+      />
+
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </header>
+  </main> `,
   styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'homes';
