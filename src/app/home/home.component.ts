38b381bd93cf99e2b44ea04bd77fc88039024346
Stefan Jaroni <sjaroni@outlook.de>
2025-02-02 22:25:19 +0100

    feat: add filtering capabilities witch template variables and ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,18 @@ import { HousingService } from '../shared/services/housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList:HousingLocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+  filterResults(text: string) {
+    if(!text) this.filteredLocationList = this.housingLocationList;
+    
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+
   }
 }
