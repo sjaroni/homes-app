e85abacdbf1d246caa40c5549e789628486c4955
Stefan Jaroni <sjaroni@outlook.de>
2025-02-02 20:12:54 +0100

    refactor: add template files for html

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -9,42 +9,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
-  template: `
-    <article>
-      <img [src]="housingLocation?.photo" alt="" class="listing-photo" />
-      <section class="listing-description">
-        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
-        <p class="listing-location">
-          {{ housingLocation?.city }}, {{ housingLocation?.state }}
-        </p>
-      </section>
-      <section class="listing-features">
-        <h2 class="section-heading">About this housing location</h2>
-        <ul>
-          <li>Units available: {{ housingLocation?.availableUnits }}</li>
-          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
-          <li>
-            Does this location have laundry: {{ housingLocation?.laundry }}
-          </li>
-        </ul>
-      </section>
-      <section class="listing-apply">
-        <h2 class="section-heading">Apply now to live here</h2>
-        <form [formGroup]="applyForm" (submit)="submitApplication()">
-          <label for="first-name">First Name</label>
-          <input type="text" id="first-name" formControlName="firstName" />
-
-          <label for="last-name">Last Name</label>
-          <input type="text" id="last-name" formControlName="lastName" />
-
-          <label for="email">Email</label>
-          <input type="email" id="email" formControlName="email" />
-
-          <button type="submit" class="primary">Apply now</button>
-        </form>
-      </section>
-    </article>
-  `,
+  templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,28 +7,14 @@ import { HousingService } from '../shared/services/housing.service';
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, HousingLocationComponent],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
-        [housingLocation]="housingLocation"
-      ></app-housing-location>
-    </section>
-  `,
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
-  constructor(){
+  constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
   }
-
 }
